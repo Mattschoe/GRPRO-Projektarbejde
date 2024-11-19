8b5bb179cd8c15dd9c645dd4d204332ae1a34190
Josva Baadsager <josba@itu.dk>
2024-11-19 15:30:14 +0100

    comments to grass

--- grpro_project/src/itumulator/simulator/NonblockingAgents/Grass.java
+++ grpro_project/src/itumulator/simulator/NonblockingAgents/Grass.java
@@ -3,4 +3,17 @@ package itumulator.simulator.NonblockingAgents;
 public class Grass {
 
 
+    public void act() {
+        // Chance to spread?
+
+
+    }
+
+    public void spread() {
+        // Look for patch of land without grass close by
+
+
+        // Create a new plot of grass at new location
+
+    }
 }
