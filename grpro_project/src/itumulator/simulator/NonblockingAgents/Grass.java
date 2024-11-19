25fb6ca810d55eace91aab8472285fff867b6293
Matthias Nielsen <matthias.s.nielsen@gmail.com>
2024-11-19 15:38:29 +0100

    Update Grass.java

--- grpro_project/src/itumulator/simulator/NonblockingAgents/Grass.java
+++ grpro_project/src/itumulator/simulator/NonblockingAgents/Grass.java
@@ -1,5 +1,27 @@
 package itumulator.simulator.NonblockingAgents;
 
-public class Grass {
+import itumulator.simulator.Actor;
+import itumulator.world.NonBlocking;
+import itumulator.world.World;
+
+public class Grass implements Actor, NonBlocking {
+    World world;
+
+    Grass() {
+
+    }
+
+    @Override
+    public void act(World world) {
+        this.world = world;
+        spread();
+    }
+
+    private void spread() {
+        for (int i = 0; i < world.getEmptySurroundingTiles().size(); i++) {
+            if
+        }
+    }
+
 
 }
