cfa19a8d568eb21e52cda6ebca2527e7cde9932e
JosvaB <josba@itu.dk>
2024-11-21 00:20:09 +0100

    Update Grass.java
    fixed accidental merge?

--- grpro_project/src/itumulator/simulator/NonblockingAgents/Grass.java
+++ grpro_project/src/itumulator/simulator/NonblockingAgents/Grass.java
@@ -36,19 +36,4 @@ public class Grass implements Actor, NonBlocking {
             }
         }
     }
-
-
-    public void act() {
-        // Chance to spread?
-
-
-    }
-
-    public void spread() {
-        // Look for patch of land without grass close by
-
-
-        // Create a new plot of grass at new location
-
-    }
 }
