d46f1f6f4f03a400e3598c819958cf7fff9bc5ed
Matthias Nielsen <matthias.s.nielsen@gmail.com>
2024-11-22 14:33:05 +0100

    Fiksede Testcases

---
+++ grpro_project/src/Testning/ParseTest.java
@@ -0,0 +1,110 @@
+package Testning;
+
+import BlockingAgents.Rabbit;
+import NonblockingAgents.Grass;
+import NonblockingAgents.RabbitBurrow;
+import itumulator.executable.DisplayInformation;
+import itumulator.executable.Program;
+import itumulator.world.Location;
+import itumulator.world.World;
+
+import java.awt.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Random;
+import java.util.Scanner;
+
+public class ParseTest {
+    File file;
+
+    //Program fields
+    int worldSize;
+    int displaySize;
+    int delay;
+
+    Program program;
+    World world;
+    Object testObject;
+
+
+    public ParseTest(File testFile, int displaySize, int delay) {
+        this.file = testFile;
+        this.delay = delay;
+        this.displaySize = displaySize;
+
+        try {
+            readFile();
+        } catch (FileNotFoundException e) {
+            System.out.println("Error!:" + e.getMessage());
+        }
+
+    }
+
+    public void readFile() throws FileNotFoundException {
+        Scanner scanner = new Scanner(file);
+
+        //Gets world size and initializes world
+        worldSize = scanner.nextInt();
+        program = new Program(worldSize, displaySize, delay);
+        world = program.getWorld();
+
+        //Display Information
+        program.setDisplayInformation(Rabbit.class, new DisplayInformation(Color.white, "rabbit-small"));
+        program.setDisplayInformation(RabbitBurrow.class, new DisplayInformation(Color.black, "hole"));
+        program.setDisplayInformation(Grass.class, new DisplayInformation(Color.green, "grass" ));
+
+        //Initializes objects in world
+        String object = "";
+        int amount = 0;
+        Random random = new Random();
+        scanner.nextLine();
+        while (scanner.hasNextLine()) {
+            //Saves object and amount
+            String line = scanner.nextLine();
+            String[] lineSplit = line.split("[-\\s]");
+
+            //If it is a amount
+            if (lineSplit.length == 2) {
+                object = lineSplit[0];
+                amount = Integer.parseInt(lineSplit[1]);
+            } else if (lineSplit.length == 3) { //If it is a min max
+                object = lineSplit[0];
+                int min = Integer.parseInt(lineSplit[1]);
+                int max = Integer.parseInt(lineSplit[2]);
+                amount = random.nextInt(max - min + 1) + min;
+            }
+
+            //Instantiate objects.
+            random = new Random();
+            Location location;
+            for (int i = 0; i < amount; i++) {
+                location = new Location(random.nextInt(worldSize), random.nextInt(worldSize));
+
+                //Finds new location if location is not empty
+                while (!world.isTileEmpty(location) || world.containsNonBlocking(location)) {
+                    location = new Location(random.nextInt(worldSize), random.nextInt(worldSize));
+                }
+
+                if (object.equals("grass")) {
+                    world.setTile(location, new Grass());
+                } else if (object.equals("rabbit")) {
+                    world.setTile(location, new Rabbit());
+                } else if (object.equals("burrow")) {
+                    world.setTile(location, new RabbitBurrow(world));
+
+                    if (world.isTileEmpty(location) && !world.containsNonBlocking(location)) {
+                        if (object.equals("grass")) world.setTile(location, new Grass());
+                        else if (object.equals("rabbit")) world.setTile(location, new Rabbit());
+                        else if (object.equals("burrow")) world.setTile(location, new RabbitBurrow(world));
+                    }
+                    testObject = world.getTile(location);
+                }
+            }
+
+        }
+    }
+
+    public void simulateTest() {
+        program.show();
+    }
+}
