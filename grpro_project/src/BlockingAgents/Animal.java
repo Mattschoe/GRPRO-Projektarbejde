f340816af7bd9b5499fd4409f0f4f2e04d64f670
MetteMy <93643480+MetteMy@users.noreply.github.com>
2024-11-25 11:11:39 +0100

    oprettede klasserne predator og animal

---
+++ grpro_project/src/BlockingAgents/Animal.java
@@ -0,0 +1,10 @@
+package BlockingAgents;
+
+import itumulator.simulator.Actor;
+import itumulator.world.World;
+
+public class Animal implements Actor {
+
+    @Override
+    public void act(World world){}
+}
---
+++ grpro_project/src/BlockingAgents/Predator.java
@@ -0,0 +1,16 @@
+package BlockingAgents;
+
+public class Predator extends Animal{
+    int strength;
+
+    Predator(int strength ){
+        this. strength = strength;
+    }
+    void hunt (){
+
+
+    }
+    void fight (){}
+
+
+}
