e5feda4e8f9c78328d74c58411126e63c550c089
Matthias Nielsen <matthias.s.nielsen@gmail.com>
2024-11-25 11:37:17 +0100

    Addede metoderne til Animal klassen, plus satte Prey op

--- grpro_project/src/BlockingAgents/Animal.java
+++ grpro_project/src/BlockingAgents/Animal.java
@@ -1,11 +1,50 @@
 package BlockingAgents;
 
 import itumulator.simulator.Actor;
+import itumulator.world.Location;
 import itumulator.world.World;
 
-public abstract class Animal implements Actor {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.Set;
 
+public abstract class Animal implements Actor {
+    World world;
+    int age;
+    int energyLevel;
+    int maxEnergy;
+    int health;
 
     @Override
     public void act(World world){}
+
+
+
+    protected World getWorld() {
+        return world;
+    }
+
+    protected void die() {
+
+    }
+
+    protected void sleep() {
+
+    }
+
+    protected void reproduce() {
+
+    }
+
+    protected void sprint() {
+
+    }
+
+    protected void move() {
+        //Moves the object to a random empty neighbour next to its location
+        Set<Location> emptyNeighbours = world.getEmptySurroundingTiles();
+        List<Location> list = new ArrayList<>(emptyNeighbours);
+        world.move(this, list.get(new Random().nextInt(list.size())));
+    }
 }
--- grpro_project/src/BlockingAgents/Prey.java
+++ grpro_project/src/BlockingAgents/Prey.java
@@ -1,4 +1,33 @@
 package BlockingAgents;
 
-public class Prey extends Animal{
+import itumulator.world.World;
+
+public abstract class Prey extends Animal {
+    World world;
+
+    Prey() {
+        super();
+        world = super.getWorld();
+    }
+
+    /***
+     * If Prey detects predator in detectPredator(), it flees.
+     */
+    protected void flee() {
+        move();
+    }
+
+    /***
+     * Detects predators in a "fleeRadius" radius.
+     */
+    private void detectPredator() {
+        int fleeRadius = 2;
+        for (Object object : world.getSurroundingTiles(fleeRadius)) {
+            if (object instanceof Predator) {
+                flee();
+            }
+        }
+    }
+
+
 }
