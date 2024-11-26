a2ac1e448c74b0bae4ff0696959f1d1c70f735dd
MetteMy <93643480+MetteMy@users.noreply.github.com>
2024-11-26 16:37:17 +0100

    Animal test

---
+++ grpro_project/src/BlockingAgents/AnimalTest.java
@@ -0,0 +1,76 @@
+package BlockingAgents;
+
+import itumulator.world.Location;
+import itumulator.world.World;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AnimalTest {
+    World w;
+    @BeforeEach
+    void setUp() {
+        w = new World(3);
+    }
+
+    @Test
+    void TestMoveTo() {
+        Rabbit rabbit = new Rabbit(w);
+        Location location = new Location(0,0);
+        Location newLocation = new Location(2,2);
+        w.setCurrentLocation(location);
+        w.setTile(location,rabbit);
+        rabbit.moveTo(newLocation); // 2 times because the distance is two, these are called over multiple calls to act.
+        rabbit.moveTo(newLocation);
+
+        assertEquals(newLocation, w.getLocation(rabbit));
+
+    }
+
+    @Test
+    void sprintTo() {
+        Rabbit rabbit = new Rabbit(w);
+        Location location = new Location(0,0);
+        Location newLocation = new Location(2,2);
+        w.setCurrentLocation(location);
+        w.setTile(location,rabbit);
+        rabbit.sprintTo(newLocation); // 1 times because the distance is two, and the rabbit is running
+
+        assertEquals(newLocation, w.getLocation(rabbit));
+
+    }
+
+    @Test
+    void moveAwayFrom() {
+        Rabbit rabbit = new Rabbit(w);
+        Location location = new Location(1,1);
+        Location enemyLocation = new Location(0,0);
+        Location expectedLocation = new Location(2,2);
+
+        w.setCurrentLocation(location);
+        w.setTile(location,rabbit);
+        rabbit.moveAwayFrom(enemyLocation);
+        assertNull(w.getTile(location));
+        assertNull(w.getTile(enemyLocation));
+        assertNotNull(w.getTile(expectedLocation));   
+
+
+
+    }
+
+    @Test
+    void move() {
+        Rabbit rabbit = new Rabbit(w);
+        Location location1 = new Location(0,0);
+        w.setCurrentLocation(location1);
+        w.setTile(location1,rabbit);
+        assertNotNull(w.getTile(location1));
+        rabbit.move();
+        Location location2 = w.getLocation(rabbit);
+        assertNotNull(w.getTile(location2));
+        assertNotEquals(location1, location2);
+        assertNull(w.getTile(location1));
+
+    }
+}
