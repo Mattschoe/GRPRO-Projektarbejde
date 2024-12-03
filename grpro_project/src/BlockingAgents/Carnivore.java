801979e1f2dbebf7ddd5def896bb92b277c405d2
MetteMy <93643480+MetteMy@users.noreply.github.com>
2024-12-03 10:24:02 +0100

    merge

--- grpro_project/src/BlockingAgents/Carnivore.java
+++ grpro_project/src/BlockingAgents/Carnivore.java
@@ -3,4 +3,7 @@ package BlockingAgents;
 import itumulator.world.Location;
 
 public interface Carnivore {
+    public void  eatMeat();
+    public void  findEatableMeat();
+    public Location getEatableMeatLocation();
 }
